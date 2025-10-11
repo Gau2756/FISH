08276a2efd604499d43e5cccf68f1d9162804286
Gau2756 <gau2756@gmail.com>
2025-10-11 14:45:13 -0400

    itmoves

--- FishGame/Assets/Scripts/PlayerMovement.cs
+++ FishGame/Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Input.GetKey()
+        // Movement Input
+        float horizontalInput = Input.GetAxis("Horizontal"); // Uses the "Horizontal" axis from Input Manager
+        rb.linearVelocity = new Vector2(horizontalInput * moveSpeed, rb.linearVelocityY);
+
+        // Jump Input
+        if (Input.GetButtonDown("Jump") && isGrounded) // Uses the "Jump" button from Input Manager
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    // Example for checking if the player is grounded (you'll need more robust checks in a full game)
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
     }
 }
